8f08158aa3509bf9e668e5b08f468e19062df2b7
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-11-21 18:50:21 +0100

    add adress.jsx

---
+++ src/components/createEmployee/Adress.jsx
@@ -0,0 +1,3 @@
+const adresses = ["123 Main St, Springfield, IL", "456 Elm St, Shelbyville, IL", "789 Oak St, Capital City, IL", "101 Maple St, Ogdenville, IL", "202 Pine St, North Haverbrook, IL"];
+
+export default adresses;
