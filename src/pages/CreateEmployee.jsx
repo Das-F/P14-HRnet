61c004c1ca649abca8bfca5af17dbdd728d1465d
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-11-24 18:24:25 +0100

    createEmployeeform added to page

--- src/pages/CreateEmployee.jsx
+++ src/pages/CreateEmployee.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/createEmployee/Header";
+import CreateEmployeeForm from "../components/createEmployeeForm/createEmployeeForm";
 import Department from "../components/createEmployee/Department";
 
 const CreateEmployee = () => {
   return (
     <>
       <Header />
+      <CreateEmployeeForm />
       <Department />
     </>
   );
