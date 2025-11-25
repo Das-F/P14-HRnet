d5c5e10a4c330a3ad5ab51b6873ad0ed7d16db95
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-11-25 17:26:27 +0100

    current employees page header added and home link

--- src/pages/CurrentEmployees.jsx
+++ src/pages/CurrentEmployees.jsx
@@ -1,13 +1,19 @@
-import Header from "../components/CreateEmployeePage/Header/Header";
+import "./CurrentEmployees.css";
+import { Link } from "react-router-dom";
 
 const CurrentEmployees = () => {
   return (
-    <div>
-      <div className="head-title">
-        <h1>HRnet</h1>
+    <>
+      <div>
+        <div className="head-title">
+          <h1>HRnet</h1>
+        </div>
+        <h1>Current Employees</h1>
       </div>
-      <h1>Current Employees</h1>
-    </div>
+      <Link to="/" className="create-employee-link">
+        Home{" "}
+      </Link>
+    </>
   );
 };
 
