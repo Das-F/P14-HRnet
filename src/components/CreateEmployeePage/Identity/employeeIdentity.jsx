6afbb92b561bef63b5d867032343c89f214078ae
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-11-27 18:52:48 +0100

    first name data uploaded in console

--- src/components/CreateEmployeePage/Identity/employeeIdentity.jsx
+++ src/components/CreateEmployeePage/Identity/employeeIdentity.jsx
@@ -5,7 +5,7 @@ function EmployeeIdentity({ dateOfBirth, startDate }) {
   return (
     <div className="employee-identity">
       <Field label="First Name">
-        <input type="text" placeholder="" />
+        <input type="text" placeholder="" onChange={(e) => console.log("First name :" + e.target.value)} />
       </Field>
       <Field label="Last Name">
         <input type="text" placeholder="" />
