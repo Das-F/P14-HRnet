67ea5dd82ed6057511af0d21becd29a6bf4e9885
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-11-28 15:37:40 +0100

    datepicker added to createEmployee page

--- src/components/CreateEmployeePage/Identity/employeeIdentity.jsx
+++ src/components/CreateEmployeePage/Identity/employeeIdentity.jsx
@@ -1,17 +1,30 @@
+import React, { useState } from "react";
 import "./employeeIdentity.css";
 import Field from "../Field/field";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-function EmployeeIdentity({ dateOfBirth, startDate }) {
+function EmployeeIdentity() {
+  const [birthDate, setBirthDate] = useState(null);
+  const [startDate, setStartDate] = useState(null);
+
   return (
     <div className="employee-identity">
       <Field label="First Name">
-        <input type="text" placeholder="" onChange={(e) => console.log("First name :" + e.target.value)} />
+        <input type="text" placeholder="" onChange={(e) => console.log("First name :", e.target.value)} />
       </Field>
+
       <Field label="Last Name">
-        <input type="text" placeholder="" />
+        <input type="text" placeholder="" onChange={(e) => console.log("Last name :", e.target.value)} />
       </Field>
-      <Field label="Date of Birth">{dateOfBirth}</Field>
-      <Field label="Start Date">{startDate}</Field>
+
+      <Field label="Date of Birth">
+        <DatePicker className="date-picker" selected={birthDate} onChange={(date) => setBirthDate(date)} dateFormat="dd/MM/yyyy" placeholderText="" />
+      </Field>
+
+      <Field label="Start Date">
+        <DatePicker className="date-picker" selected={startDate} onChange={(date) => setStartDate(date)} dateFormat="dd/MM/yyyy" placeholderText="" />
+      </Field>
     </div>
   );
 }
