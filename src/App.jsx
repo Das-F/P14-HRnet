89c91ae000672c3d76f950efa6acd479d25d1c0a
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-12-11 14:09:35 +0100

    router corrected and pop-up modal created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import CreateEmployee from "./pages/CreateEmployee";
 import CurrentEmployees from "./pages/CurrentEmployees";
 import "./App.css";
--- src/pages/CreateEmployee.jsx
+++ src/pages/CreateEmployee.jsx
@@ -1,17 +1,26 @@
 import "./CreateEmployee.css";
+import { useState } from "react";
 import Header from "../components/CreateEmployeePage/Header/Header";
 import CreateEmployeeForm from "../components/CreateEmployeePage/createEmployeeForm/createEmployeeForm";
 import SaveButton from "../components/CreateEmployeePage/saveButton/saveButton";
+import CreationPopUp from "../components/CreateEmployeePage/CreationPopUp/CreationPop-up";
 
 const CreateEmployee = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
   const handleClick = () => {
-    console.log("Save button clicked");
+    // Affiche la modale de confirmation
+    setShowPopup(true);
   };
+
+  const handleClose = () => setShowPopup(false);
+
   return (
     <>
       <Header />
       <CreateEmployeeForm />
       <SaveButton onSave={handleClick} />
+      {showPopup && <CreationPopUp onClose={handleClose} />}
     </>
   );
 };
